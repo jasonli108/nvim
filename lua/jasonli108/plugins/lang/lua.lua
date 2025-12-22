f3ecedb8ca5f6c5ec2ff05d502cc7c8c1e481627
JasonLi108 <jasonli108@gmail.com>
2025-12-22 16:05:24 -0500

    fix lua_ls not attached issue

--- lua/jasonli108/plugins/lang/lua.lua
+++ lua/jasonli108/plugins/lang/lua.lua
@@ -1,55 +1,60 @@
 local lsp_server = "lua_ls"
 
 return {
-  -- 1. Tools (Mason)
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "stylua", "shfmt", lsp_server })
-    end,
-  },
+	-- 1. Tools (Mason)
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		opts = function(_, opts)
+			opts.ensure_installed = opts.ensure_installed or {}
+			vim.list_extend(opts.ensure_installed, { "stylua", "shfmt", lsp_server })
+		end,
+	},
 
-  -- 2. LSP Settings (The LazyVim way)
-  {
-    "neovim/nvim-lspconfig",
-    opts = function(_, opts)
-      opts.servers = opts.servers or {}
-      
-      -- Inject settings for lua_ls into the central opts table
-      opts.servers[lsp_server] = {
-        settings = {
-          Lua = {
-            runtime = { version = "LuaJIT" },
-            diagnostics = { globals = { "vim" } },
-            workspace = { 
-              library = vim.api.nvim_get_runtime_file("", true), 
-              checkThirdParty = false 
-            },
-            telemetry = { enable = false },
-            hint = { enable = true }, -- 2025: Enable inlay hints for Lua
-          },
-        },
-      }
-    end,
-  },
+	-- 2. LSP Settings (The LazyVim way)
+	{
+		"neovim/nvim-lspconfig",
+		opts = function(_, opts)
+			opts.servers = opts.servers or {}
 
-  -- 3. Formatter
-  {
-    "stevearc/conform.nvim",
-    opts = function(_, opts)
-      opts.formatters_by_ft = opts.formatters_by_ft or {}
-      opts.formatters_by_ft.lua = { "stylua" }
-    end,
-  },
+			opts.servers.lua_ls = {
+				-- Fix the "not executable" warning by pointing directly to Mason
+				cmd = { vim.fn.stdpath("data") .. "/mason/bin/lua-language-server" },
 
-  -- 4. Treesitter
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "lua", "luadoc", "luap" })
-    end,
-  },
+				-- Fix the "expected table, got function" error
+				-- Use root_markers (table) instead of root_dir (function) for 0.11 compatibility
+				root_markers = { ".git", ".luarc.json", ".luarc.jsonc", "init.lua" },
+
+				settings = {
+					Lua = {
+						runtime = { version = "LuaJIT" },
+						diagnostics = { globals = { "vim" } },
+						workspace = {
+							library = { vim.env.VIMRUNTIME },
+							checkThirdParty = false,
+						},
+						telemetry = { enable = false },
+						hint = { enable = true },
+					},
+				},
+			}
+		end,
+	},
+
+	-- 3. Formatter
+	{
+		"stevearc/conform.nvim",
+		opts = function(_, opts)
+			opts.formatters_by_ft = opts.formatters_by_ft or {}
+			opts.formatters_by_ft.lua = { "stylua" }
+		end,
+	},
+
+	-- 4. Treesitter
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			opts.ensure_installed = opts.ensure_installed or {}
+			vim.list_extend(opts.ensure_installed, { "lua", "luadoc", "luap" })
+		end,
+	},
 }
-
